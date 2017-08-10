c2e409ab2f6110b4ad87f2bd810060c726571e5a
Carlos Barrera <carlostonatihu@gmail.com>
2017-08-10 18:39:10 -0500

    hola

---
+++ HolaMundo/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "C:/Users/reymy/Documents/quinto/arquitortura/HolaMundo/prueba_isim_beh.exe" -prj "C:/Users/reymy/Documents/quinto/arquitortura/HolaMundo/prueba_beh.prj" "work.prueba" 
