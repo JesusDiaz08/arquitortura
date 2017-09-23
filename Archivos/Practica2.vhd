3316c24890dcdca17a62bd82ca75e89d37676ca2
Andres Saldana <pollyketpo@gmail.com>
2017-09-22 23:28:11 -0500

    Mas archivos sin subir LeL

--- Archivos/Practica2.vhd
+++ Archivos/Practica2.vhd
@@ -52,18 +52,17 @@ constant NUM1: 			std_logic_vector(15 downto 0):= X"00001"; --hexadecimal
 
 
 
-architecture memoria of programa is
-type memoria is array (o to 2++16-1) of std_logic_vector(24 downto 0);
+architecture a_programa of programa is
+type memoria is array (o to 2+16-1) of std_logic_vector(24 downto 0);
 constant mem_prog: memoria :=(
 		opcode_LI&R0&NUM1, 						--LI RO #1
 		opcode_LI&R1&X"0009",				    --LI R1 #7
 		opcode_tipoR&R1&R1&R0&SU&funcode_ADD, 	--ADD R1,R1,R0
 		opcode_SWI&R1&X"0005", 					--SWI R1,5
 		opcode_B&SU&X"0002", 					--B ciclo
-		others=> (others => '0');	--others the localidades y su tamaño.
-		
+		others=> (others => '0');	--others the localidades y su tamanio.	
 );
 begin
 
-end memoria;
+end a_programa;
 
