de8e5c685d6c87da461b096956f1306ccd31bd2d
Andres Saldana <pollyketpo@gmail.com>
2017-10-17 22:49:16 -0500

    Puebas listas

--- Memoriadedatos/MemoriaD.vhd
+++ Memoriadedatos/MemoriaD.vhd
@@ -3,16 +3,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity MemoriaD is
-    Port ( DIN   : in  STD_LOGIC_VECTOR (7 downto 0);
-			  DOUT  : out  STD_LOGIC_VECTOR (7 downto 0);
-           ADR_WR : in  STD_LOGIC_VECTOR (15 downto 0);
-           WREN  : in  STD_LOGIC; --WRITE ENABLE
-           CLK   : in  STD_LOGIC
+	 GENERIC(
+		DATA_N: INTEGER:=16;
+		ADDR_N: INTEGER:=8
+	 );
+    Port ( DIN    : in  STD_LOGIC_VECTOR (DATA_N-1 downto 0);
+			  DOUT   : out  STD_LOGIC_VECTOR (DATA_N-1 downto 0);
+           ADR_WR : in  STD_LOGIC_VECTOR (ADDR_N-1 downto 0);
+           WREN   : in  STD_LOGIC; --WRITE ENABLE
+           CLK    : in  STD_LOGIC
            );
 end MemoriaD;
 
 architecture Behavioral of MemoriaD is
-TYPE MEMORIA IS ARRAY (0 to 2**16-1) OF STD_LOGIC_VECTOR (7 DOWNTO 0); --checar la organizacion, por el momento es 16x8
+TYPE MEMORIA IS ARRAY (0 to 2**ADDR_N-1) OF STD_LOGIC_VECTOR (DATA_N-1 DOWNTO 0); --checar la organizacion, por el momento es 16x8
 SIGNAL RAM_DIST : MEMORIA;
 begin
 	PRAM: PROCESS(CLK)
--- Memoriadedatos/TB_MemoriaD.vhd
+++ Memoriadedatos/TB_MemoriaD.vhd
@@ -1,30 +1,3 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   13:44:59 10/14/2017
--- Design Name:   
--- Module Name:   C:/Users/ANDRES/Documents/GitHub/arquitortura/Memoriadedatos/TB_MemoriaD.vhd
--- Project Name:  Memoriadedatos
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: MemoriaD
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 LIBRARY STD;
 USE ieee.std_logic_1164.ALL;
@@ -36,6 +9,10 @@ USE ieee.std_logic_TEXTIO.ALL;
 --USE ieee.numeric_std.ALL;
  
 ENTITY TB_MemoriaD IS
+	 GENERIC(
+		DATA_N: INTEGER:=16;
+		ADDR_N: INTEGER:=8
+	 );
 END TB_MemoriaD;
  
 ARCHITECTURE behavior OF TB_MemoriaD IS 
@@ -44,9 +21,9 @@ ARCHITECTURE behavior OF TB_MemoriaD IS
  
     COMPONENT MemoriaD
     PORT(
-         DIN : IN  std_logic_vector(7 downto 0);
-         DOUT : OUT  std_logic_vector(7 downto 0);
-         ADR_WR : IN  std_logic_vector(15 downto 0);
+         DIN : IN  std_logic_vector(DATA_N-1 downto 0);
+         DOUT : OUT  std_logic_vector(DATA_N-1 downto 0);
+         ADR_WR : IN  std_logic_vector(ADDR_N-1 downto 0);
          WREN : IN  std_logic;
          CLK : IN  std_logic
         );
@@ -54,16 +31,16 @@ ARCHITECTURE behavior OF TB_MemoriaD IS
     
 
    --Inputs
-   signal DIN : std_logic_vector(7 downto 0) := (others => '0');
-   signal ADR_WR : std_logic_vector(15 downto 0) := (others => '0');
+   signal DIN : std_logic_vector(DATA_N-1 downto 0) := (others => '0');
+   signal ADR_WR : std_logic_vector(ADDR_N-1 downto 0) := (others => '0');
    signal WREN : std_logic := '0';
    signal CLK : std_logic := '0';
 
  	--Outputs
-   signal DOUT : std_logic_vector(7 downto 0);
+   signal DOUT : std_logic_vector(DATA_N-1 downto 0);
 
    -- Clock period definitions
-   constant CLK_period : time := 10 ns;
+   constant CLK_period : time := 20 ns;
  
 BEGIN
  
@@ -91,16 +68,16 @@ BEGIN
 	--Archivos y variables de salida
 	file ARCH_RES         : TEXT;
 	variable LINEA_RES    : line;
-	VARIABLE VAR_DOUT : STD_LOGIC_VECTOR(7 DOWNTO 0);
+	VARIABLE VAR_DOUT : STD_LOGIC_VECTOR(DATA_N-1 DOWNTO 0);
 	--Archivos y variables de entrada
 	file ARCH_DATOS: TEXT;
 	variable LINEA     : line;
-   variable VAR_DIN   : STD_LOGIC_VECTOR(7 DOWNTO 0);
-	variable VAR_ADR_WR : STD_LOGIC_VECTOR(15 DOWNTO 0);
+   variable VAR_DIN   : STD_LOGIC_VECTOR(DATA_N-1 DOWNTO 0);
+	variable VAR_ADR_WR : STD_LOGIC_VECTOR(ADDR_N-1 DOWNTO 0);
 	variable VAR_WREN  : STD_LOGIC;
 		
 		  --
-	VARIABLE CADENA : STRING(1 TO 9);
+	VARIABLE CADENA : STRING(1 TO 5);
 	VARIABLE ESPACIO : STRING(1 TO 2);
 		  --
 		
@@ -108,21 +85,21 @@ BEGIN
 	file_open(ARCH_DATOS, "DATOS.TXT", READ_MODE);
    file_open(ARCH_RES, "RESULTADOS.TXT", WRITE_MODE);
 
-        CADENA := "DIN      ";
+        CADENA := "DIN  ";
         write(LINEA_RES, CADENA, right, CADENA'LENGTH);  
-		  CADENA := "   ADR_WR";
-        write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);  
-		  CADENA := "    WREN ";
-        write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);  
-		  CADENA := "  DOUT   ";
-        write(LINEA_RES, CADENA, right, CADENA'LENGTH+1); 
+		  CADENA := "ADRWR";
+        write(LINEA_RES, CADENA, right, CADENA'LENGTH);  
+		  CADENA := " WREN";
+        write(LINEA_RES, CADENA, right, CADENA'LENGTH);  
+		  CADENA := " DOUT";
+        write(LINEA_RES, CADENA, right, CADENA'LENGTH); 
         writeline(ARCH_RES,LINEA_RES);-- escribe la linea en el archivo
 
-        for i in 0 to 1 loop
+        for i in 0 to 11 loop
 		  
 				readline(ARCH_DATOS, LINEA);
 
-				read(LINEA, VAR_DIN); --Leemos el din
+				Hread(LINEA, VAR_DIN); --Leemos el din
 				DIN<= VAR_DIN;
 				Hread(LINEA, VAR_ADR_WR); -- Leemos la direccion a donde se escribira (en hexadecimal)
 				ADR_WR<= VAR_ADR_WR;
@@ -132,13 +109,12 @@ BEGIN
 				wait until rising_edge(CLK);
 				VAR_DOUT := DOUT;
 		
-				write(LINEA_RES, VAR_DIN ,right, 5);
-				write(LINEA_RES, ESPACIO ,right, 4);
-				hwrite(LINEA_RES, VAR_ADR_WR ,right, 5);
-				write(LINEA_RES, ESPACIO ,right, 5);
-				write(LINEA_RES, VAR_WREN ,right, 5);
-				write(LINEA_RES, ESPACIO ,right, 5);
-			   write(LINEA_RES, VAR_DOUT ,right, 5);	--ESCRIBE EL CAMPO DOUT			
+				hwrite(LINEA_RES, VAR_DIN ,right, 3);
+				write(LINEA_RES, ESPACIO ,right, 2);
+				hwrite(LINEA_RES, VAR_ADR_WR ,right, 3);
+				write(LINEA_RES, VAR_WREN ,right, 4);
+				write(LINEA_RES, ESPACIO ,right, 3);
+			   hwrite(LINEA_RES, VAR_DOUT ,right, 3);	--ESCRIBE EL CAMPO DOUT			
 				writeline(ARCH_RES,LINEA_RES);-- escribe la linea en el archivo
 				  
 		  end loop;
